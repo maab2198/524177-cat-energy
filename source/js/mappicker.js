fc399c382811801e4eab074164b7ae1c9f6ac927
maab2198 <maab2198@gmail.com>
2018-04-28 13:53:08 +0300

    path to pin

--- source/js/mappicker.js
+++ source/js/mappicker.js
@@ -10,12 +10,12 @@ function initMap() {
 
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(59.938931, 30.323089),
-    icon: '../img/map-pin-small.png',
+    icon: 'img/map-pin-small.png',
     map: map
   });
 
   if (window.innerWidth >= 768) {
-    marker.icon = '../img/map-pin.png';
+    marker.icon = 'img/map-pin.png';
     if (window.innerWidth >= 1300) {
       map.center = new google.maps.LatLng(59.939014, 30.319356);
     }
