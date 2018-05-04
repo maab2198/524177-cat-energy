4e4f6070c27503d9e517bd4dc99de513a6ca8b78
maab2198 <maab2198@gmail.com>
2018-05-04 11:31:03 +0300

    green promo && map

--- source/js/mappicker.js
+++ source/js/mappicker.js
@@ -1,28 +1,52 @@
-window.addEventListener("resize", changeIcon, false);
+window.addEventListener("resize", isMobileDevice, false);
+
+function isMobileDevice() {
+  if (
+    (typeof window.orientation !== "undefined") ||
+    (navigator.userAgent.indexOf('IEMobile') !== -1)
+    )
+    { console.log("phone");
+      return;
+    }
+  resize();
+};
+
+var myLatlng = {
+  center: { lat: 59.938931, lng: 30.323089 },
+  right: { lat: 59.939014, lng: 30.319356 }
+};
+
 var map;
+var marker;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
-    center: new google.maps.LatLng(59.938931, 30.323089),
+    center: myLatlng.center,
     mapTypeId: 'roadmap'
   });
 
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(59.938931, 30.323089),
+  marker = new google.maps.Marker({
+    position: myLatlng.center,
     icon: 'img/map-pin-small.png',
     map: map
   });
-  changeIcon();
-
+  isMobileDevice();
 }
 
-function changeIcon() {
-    if (window.innerWidth >= 768) {
-    marker.icon = 'img/map-pin.png';
+function resize() {
     if (window.innerWidth >= 1300) {
-      map.center = new google.maps.LatLng(59.939014, 30.319356);
-    }
-  }
+      map.panTo(myLatlng.right);
 
+  }
+  else {
+    map.panTo(myLatlng.center);
+  }
+      marker.icon = 'img/map-pin.png';
 }
+
+// isMobileDevice()
+//https://coderwall.com/p/i817wa/one-line-function-to-detect-mobile-devices-with-javascript
+//при медленом интернете на телефоне все время будет рендериться карта заново при каждом касании экрана
+// -> resize работает только если не телефон
+// маленькая иконка только на телефоне
